3d1f784646659414d6af8a0f868a3b60e32c7bb3
zgf <guofengzheng1@creditease.cn>
2017-04-16 20:45:09 +0800

    test stream

---
+++ springclound-stream/src/main/java/com/zgf/springclound/stream/sync/source/TestSyncSteam.java
@@ -0,0 +1,77 @@
+package com.zgf.springclound.stream.sync.source;
+
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.binding.BinderAwareChannelResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.router.ExpressionEvaluatingRouter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collections;
+import java.util.HashMap;
+
+/**
+ * Created by zgf on 17/4/15.
+ */
+@EnableBinding
+@Controller
+public class TestSyncSteam {
+    private Logger logger = LoggerFactory.getLogger(TestSyncSteam.class);
+
+    @Autowired
+    private BinderAwareChannelResolver resolver;
+
+//    @Autowired
+//    @Qualifier("sourceChannel")
+//    private MessageChannel localChannel;
+
+//@Bean(name = "sourceChannel")
+//public MessageChannel localChannel() {
+//    return new QueueChannel();
+//}
+
+    @RequestMapping(path = "/{id}")
+    @ResponseBody
+    public String handleRequest(@PathVariable String id) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("id", id);
+        sendMessage(map, "application/json");
+        return "SUCCESS";
+    }
+
+    private void sendMessage(Object body, Object contentType) {
+        QueueChannel queueChannel = new QueueChannel();
+        queueChannel.send(MessageBuilder.createMessage(body,
+                new MessageHeaders(Collections.singletonMap(MessageHeaders.CONTENT_TYPE, contentType))));
+        Message<?> receive = queueChannel.receive(60000);
+        System.out.println("received: " + JSON.toJSONString(receive.getPayload()));
+    }
+
+//    @ServiceActivator(inputChannel = "sourceChannel")
+//    public void loggerSink(Object payload) {
+//        logger.info("Received: " + payload);
+//    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "sourceChannel")
+    public ExpressionEvaluatingRouter router() {
+        ExpressionEvaluatingRouter router = new ExpressionEvaluatingRouter(new SpelExpressionParser().parseExpression("payload.id"));
+        router.setDefaultOutputChannelName("zgf-test-sync");
+        router.setChannelResolver(resolver);
+        return router;
+    }
+}
+
+
---
+++ springclound-stream/src/main/java/com/zgf/springclound/stream/sync/source/TestSyncStreamApplication.java
@@ -0,0 +1,14 @@
+package com.zgf.springclound.stream.sync.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zgf on 17/4/15.
+ */
+@SpringBootApplication
+public class TestSyncStreamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestSyncStreamApplication.class, args);
+    }
+}
